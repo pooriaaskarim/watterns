d186d472e1eb767f346727856586bfd82211913e
Pooria Askari Moqaddam <pooriaaskarim@gmail.com>
2024-02-02 13:41:50 +0330

    Simplified interfaces a little Minor Bug fix

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from services import WeatherService
 
 def main() -> None:
     weather_service = WeatherService()
-    # weather_service.service.how_is(location='shiraz')
-    weather_service.interface.get(location='shiraz')
+    # weather_service.interface.how_is(location='shiraz')
+    weather_service.interface.get_rainy_days(location='tonekabon')
 
 
 if __name__ == '__main__':
--- services/weather/interfaces/wttr/interface.py
+++ services/weather/interfaces/wttr/interface.py
@@ -1,37 +1,50 @@
-from services.weather.interfaces.i_weather_interface import IWeatherInterface, IWeatherApi
+import requests
+
+from services.weather.interfaces.i_weather_interface import IWeatherInterface
 from .models.location_weather import *
 import json
-from types import SimpleNamespace
-
-
-class WttrApi(IWeatherApi):
-    def __init__(self):
-        super().__init__('https://wttr.in/')
-
-    def how_is(self, location: str) -> str:
-        return f'{self.api_base}{location}'
-
-    def get(self, location: str) -> str:
-        return f'{self.api_base}{location}0?format=j1'
 
 
 class WttrInterface(IWeatherInterface):
     def __init__(self):
-        super().__init__(api=WttrApi())
+        super().__init__(api_base='https://wttr.in/')
 
     def how_is(self, location: str, locale: str or None = None) -> None:
-        super().how_is(location=location, locale=locale)
+        try:
+            response = requests.get(
+                f'{self.api_base}{location}',
+                headers={'accept-language': locale}, )
+            print(response.text)
+        except Exception as e:
+            print(f'Sh$%t Happened\n\t{e}')
 
-    def get(self, location: str):
-        data = super().get(location)
-        x = json.loads(data.text)
-        wttr = Wttr.from_dict(x)
-        probableRainyDays = []
-        for w in wttr.weather:
-            for h in w.hourly:
-                print(w.date + '/' + h.time + '\t\t\t -----> ' + h.chanceofrain + ' : ' + [f'{desc.value}' for desc in h.weatherDesc].__str__())
-                if int(h.chanceofrain) > 0:
-                    probableRainyDays.append(
-                        {f'{w.date}/{h.time}:{h.time}': h.chanceofrain})
+    def get_rainy_days(self, location: str, locale: str or None = None):
+        response = requests.get(
+            f'{self.api_base}{location}?format=j1',
+            headers={'accept-language': locale},
+        )
+        data = json.loads(response.text)
+        wttr = Wttr.from_dict(data)
+        days = [weather for weather in wttr.weather]
+        probable_rainy_days = filter(
+            lambda day: any(int(hourly.chanceofrain) > 0 for hourly in day.hourly), days)
 
-        print(probableRainyDays)
+        new_line = '\n'
+        tab = '\t'
+
+        print(f"""
+{location.capitalize()}:{tab.join(
+            [
+                f'{new_line}{tab}{rainy_day.date}:{new_line}{tab}{tab}'
+                + f'{new_line}{tab}{tab}'.join(
+                    [
+                        f'Raining chance of %{hour.chanceofrain.ljust(3, " ")}'
+                        f' at {hour.time.rjust(4, "0")[:2]}:00'
+                        for hour in rainy_day.hourly if int(hour.chanceofrain) > 0
+                    ]
+                )
+                for rainy_day in probable_rainy_days
+            ]
+        )
+        }
+""")
